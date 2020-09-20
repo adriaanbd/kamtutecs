c6722f04237c51c234d96b8a0c0c2c043eaac43e
Adriaan Beiertz <adriaanbd@gmail.com>
2020-09-19 22:58:15 -0500

    refactores from function to arrow funcs

--- src/components/Upload.js
+++ src/components/Upload.js
@@ -1,8 +1,8 @@
 import React from "react";
 import Button from "react-bootstrap/Button";
 
-function Upload() {
-  function handleUpload() {
+const Upload = () => {
+  const handleUpload = () => {
     console.log("Hello");
   }
   return (
